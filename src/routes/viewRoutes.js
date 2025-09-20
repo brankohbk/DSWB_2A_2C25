0c9ee2e7372557c50edd8d6c88d72d5668332f34
Branko Haberkon <brankoh@gmail.com>
2025-09-19 22:52:03 -0300

    feat: Fetch all Empleados on create Turno or edit Turno

--- src/routes/viewRoutes.js
+++ src/routes/viewRoutes.js
@@ -42,15 +42,21 @@ router.get('/turnos', async (req, res,next) => {
     
 });
 
-router.get('/turnos/nuevo', (req, res) => {
-    res.render('turnos/new', { title: 'Nuevo turno' });
+router.get('/turnos/nuevo', async (req, res) => {    
+    try{
+        const empleados = await Empleado.getAll();
+        res.render('turnos/new', { title: 'Nuevo turno', empleados, ok: req.query.ok });
+    } catch (e) {
+        next(e);
+    }
 });
 
 router.get('/turnos/:id/actualizar', async (req, res, next) => {
     try{
+        const empleados = await Empleado.getAll();
         const turno = await Turno.getById(req.params.id);
         if (!turno) return res.status(404).render('404', { title:'Turno inexistente' });
-        res.render('turnos/actualizar', { title: 'Actualizar turno', turno});
+        res.render('turnos/actualizar', { title: 'Actualizar turno', turno, empleados});
     } catch (e) {
         next(e);
     }
